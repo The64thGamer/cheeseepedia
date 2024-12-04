cf1cb678c8c0e89e8c7df22e4770de78b11ef848
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-12-04 14:22:14 -0700

    More and more and more fixes never stops!!!!

--- static/admin/cms-preview.js
+++ static/admin/cms-preview.js
@@ -1,23 +1,19 @@
-function registerHTMLPreviewTemplate(collectionName) {
-  CMS.registerPreviewTemplate(collectionName, ({ entry }) => {
-    const data = entry.getIn(['data']);
-    return `
-      <article>
+CMS.registerPreviewTemplate(collectionName, ({ entry }) => {
+  const data = entry.getIn(['data']);
+  return React.createElement('article', {
+    dangerouslySetInnerHTML: {
+      __html: `
         <h1>${data.get('title')}</h1>
         <div>${data.get('body')}</div>
-      </article>
-    `;
+      `
+    }
   });
-}
+});
+
 const items = [
-  "Animatronic Shows", "Stage Variations", "Animatronics", "Retrofits", "Costumed Characters", 
-  "Animatronic Repair & Preservation", "Showtapes", "Showtape Formats", "Live Shows", 
-  "Showtape Puppets", "Family Vision", "ShowBiz Pizza Programs", "Merchandise", "Menu Items", 
-  "Tokens", "Tickets", "Movies", "Video Games", "Locations", "Arcade Games", "Employee Wear", 
-  "Remodels & Initiatives", "Store Fixtures", "Ad Vehicles", "Animatronic Control Systems", 
-  "Other Systems", "Programming Systems", "Animatronic Control Systems", "Promotional Material", 
-  "Documents", "Corporate Documents", "Training, Updates, & Company Media", "Photos", "Videos", 
-  "News Footage", "Commercials", "History", "Events", "Characters"
+  "admin", "videos", "photos", 
+  "meta", "users", "transcriptions", 
+  "wiki"
 ];
 
 items.forEach(item => {
