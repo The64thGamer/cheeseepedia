887f78816fc26bd7e284f2fb900d63b8a138b35d
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-12-08 11:58:41 -0700

    Preview template fix?

--- static/admin/cms-preview.js
+++ static/admin/cms-preview.js
@@ -1,1 +1,16 @@
 CMS.registerPreviewStyle("/themes/sixtyth-fortran/assets/css/main.css");
+
+
+var PostPreview = createClass({
+  render: function() {
+    var entry = this.props.entry;
+    var image = entry.getIn(['data', 'image']);
+    var bg = this.props.getAsset(image);
+    return h('div', {},
+      h('h1', {}, entry.getIn(['data', 'title'])),
+      h('div', {"className": "article-section"}, this.props.widgetFor('body'))
+    );
+  }
+});
+
+CMS.registerPreviewTemplate("wiki", PostPreview);
