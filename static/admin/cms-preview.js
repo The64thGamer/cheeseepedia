fe4220c2d8bf011bb8e4caea742b728c0b2eaf67
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-12-08 14:52:40 -0700

    test

--- static/admin/cms-preview.js
+++ static/admin/cms-preview.js
@@ -5,9 +5,14 @@ var PostPreview = createClass({
     var entry = this.props.entry;
     var image = entry.getIn(['data', 'image']);
     var bg = this.props.getAsset(image);
+    var bodyHTML = this.props.widgetFor('body').props.value; // Access raw HTML
+
     return h('div', {},
       h('h1', {}, entry.getIn(['data', 'title'])),
-      h('div', {"className": "article-section"}, this.props.widgetFor('body'))
+      h('div', {
+        "className": "article-section",
+        "dangerouslySetInnerHTML": { __html: bodyHTML } // Render raw HTML
+      })
     );
   }
 });
